75955ddadf4e2ee4ea21258fbe441f9f31557f48
Pedro Sanz <psanzm@sescam.jccm.es>
2022-10-24 12:13:06 +0200

    Fin Seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+    
+    //debugger;
+    let numero = 0;
+    numero = numero + 10;
+    numero = 30;
+    numero = numero - 15;
+
+    console.log(numero);
+  }
 
 }
